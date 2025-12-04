0b2fef3d8f29227f949df76e9bc47b2a3b151061
Ana C <anacalen03@gmail.com>
2025-12-04 14:09:16 +0100

    new portfolio web

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Siempre empezar arriba al recargar o cambiar de página
+window.history.scrollRestoration = "manual";
+
+window.addEventListener("beforeunload", () => {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
